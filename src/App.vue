ca73dc307a3fe60e35a1f248c756ecb234ee7e16
Hyun jin <blackduvet52@gmail.com>
2023-12-08 19:29:08 +0900

    VUE Study Update

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<!-- Vue는 3부분으로 나눠짐 
+     1. template
+     2. script - javascript 로직이 들어감
+     3. style -
+-->
+<template>
+  <div class="name">{{ name }}</div>
+  <duv>123</duv>
+</template>
+<script>
+export default {
+  setup() {
+    const name = "Kossie Coder";
+
+    // 원하는 변수들은 template에서 접근이 가능
+    return {
+      name,
+    };
+  },
+};
+</script>
+<style>
+.name {
+  color: red;
+}
+</style>
