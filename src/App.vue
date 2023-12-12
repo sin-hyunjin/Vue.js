7ba1bf28f904fffcbabd93b3e77560229c30a6f0
Hyun jin <blackduvet52@gmail.com>
2023-12-12 12:56:00 +0900

    Vue Update
    Vue Study

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <h1>실습</h1>
+  <input id="input" type="text" v-bind:placeholder="message" />
+</template>
+
+<script>
+const app = Vue.createApp({
+  data() {
+    return {
+      message: "값을 입력해주세요!",
+    };
+  },
+});
+app.mount("#input");
+</script>
+
+<style scoped></style>
